953f6b2ccca16bbc103413c93fe2d4a1ebeeeb95
AshaVaddi <ashavaddi6303@gmail.com>
2024-07-22 22:30:55 +0530

    Create lastcol2.py

---
+++ lastcol2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Example 2-dimensional Numpy array (matrix)
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+# Extract the last column of the matrix using numpy.take
+last_column = np.take(matrix, -1, axis=1)
+
+print("Original matrix:")
+print(matrix)
+print("\nLast column of the matrix:")
+print(last_column)
